1e8981015893205e9a6336d25e239dc99580e544
Abdul Hakeem Shaik <50977399+abdulhakeem143@users.noreply.github.com>
2023-02-25 10:19:26 +0530

    first commit

---
+++ FileNameChallenge.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class FileNameChallenge{
+    public static void main(String[] args) throws FileNotFoundException {
+        File fileName = new File("inputFile.txt");
+        Scanner read = new Scanner(fileName);
+
+        // String[] fileNames = {"File1", " File2", "  File3", "File4", "File5"};
+        String[] fileNames = new String[5];
+        for(int i = 0; i<fileNames.length; i++){
+            fileNames[i] = read.nextLine();
+        }
+
+        // Create a map to store the parent-child relationships
+        Map<String, String> fileMap = new HashMap<>();
+        fileMap.put("File1", null);
+
+        for (int i = 1; i < fileNames.length; i++) {
+            String currFile = fileNames[i].trim();
+            int numSpaces = fileNames[i].length() - currFile.length();
+
+            if (numSpaces == 1) {
+                fileMap.put(currFile, "File1");
+            } else if (numSpaces > 1) {
+                String parentFile = fileNames[i-1].trim();
+                fileMap.put(currFile, parentFile);
+            } else {
+                fileMap.put(currFile, null);
+            }
+        }
+
+        // Print the output
+        System.out.printf("%-10s %-10s\n", "FileName", "ParentFile");
+        System.out.println("========== ==========");
+        for (String file : fileMap.keySet()) {
+            System.out.printf("%-10s %-10s\n", file, fileMap.get(file));
+        }
+    }
+}
